f88ab6f0fcc492ba4c777c7b6520a2099eda7ffe
Dagim <dagimgetaW27@gmail.com>
2024-04-12 20:35:20 +0300

    humberg menu func

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector("#menu__btn");
+const navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
